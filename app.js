fd6b8ca161961998f6ccdeb6574773f5e5150440
Kenneth Austin <s0574052@htw-berlin.de>
2022-03-06 20:04:44 +0100

    barcode kann eingelesen werden

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 let DBR = require("dynamsoft-node-barcode");
+(async()=>{
+    let reader = await DBR.BarcodeReader.createInstance();
+    let results = await reader.decode('C:\\Users\\kenne\\Desktop\\Barcode Scanner\\Testaufgabe-1.jpg');
+    for(let result of results){
+        console.log(result.barcodeText);
+    }
+    reader.destroy();
+    await DBR.BarcodeReader._dbrWorker.terminate();
+    process.exit();
+})();
